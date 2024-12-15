a227f73295f03859d92c60b186e4aa0bdb999b72
Bruno Bechara <Brunosbechara@gmail.com>
2024-12-14 23:01:20 -0300

    fix: Implementação das rotas consumindo os casos de uso

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications.controller';
 import { SendNotification } from 'src/application/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
+import { CountRecipientNotification } from 'src/application/use-cases/count-recipient-notification';
+import { GetRecipientNotification } from 'src/application/use-cases/get-recipient-notification';
+import { ReadNotification } from 'src/application/use-cases/read-notification';
+import { UnreadNotification } from 'src/application/use-cases/unread-notification';
+import { CancelNotification } from 'src/application/use-cases/cancel-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotification,
+    GetRecipientNotification,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
